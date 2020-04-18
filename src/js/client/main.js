2749600c0cb436dcecc3fb07c4b2a77d0b5332ee
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-04-17 22:39:58 -0700

    Bingo! Got the socketio server going :)

--- src/js/client/main.js
+++ src/js/client/main.js
@@ -1,1 +1,18 @@
-console.log('hello!');
+import io from 'socket.io-client';
+
+const serverAddress = 'localhost:3000'
+let socket = null;
+
+function init() {
+	initSocketIo();
+}
+
+function initSocketIo() {
+	socket = new io(serverAddress)
+
+	socket.on('connect', () => {
+		console.log('connected to server!');
+	});
+}
+
+window.onload = init;
