4bf412383f8afabb09b2600adcea85e97252a17e
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-04-17 21:35:25 -0700

    Ok! The client and server webpack stuff seem to be set up now!

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+const common = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    mode: 'development'
+}
+
+const client = {
+    entry: './src/js/client/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build/client/js'),
+        filename: 'main.bundle.js'
+    },
+    devtool: 'source-map'
+}
+
+const server = {
+    entry: './src/js/server/server.js',
+    output: {
+        path: path.resolve(__dirname, 'build/server'),
+        filename: 'server.bundle.js'
+    },
+    target: 'node',
+    externals: [nodeExternals()]
+}
+
+module.exports = [
+    Object.assign({}, common, client),
+    Object.assign({}, common, server),
+];
